a2718c251e6a326fa9a5062b978701954baca01c
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-02-20 11:32:50 -0800

    implemented FunctionLibrary

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,6 +8,9 @@ public class Graph : MonoBehaviour {
     [SerializeField, Range(10, 100)]
 	int resolution = 10;
 
+    [SerializeField]
+	FunctionLibrary.FunctionName function;
+
     Transform[] points;
 
     void Awake () {
@@ -26,11 +29,12 @@ public class Graph : MonoBehaviour {
 	}
 
     void Update () {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
 		for (int i = 0; i < points.Length; i++) {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = 1.5f * Mathf.Sin(Mathf.PI * (position.x + time));
+            position.y = 1.5f * f(position.x, time);
             point.localPosition = position;
         }
 	}
