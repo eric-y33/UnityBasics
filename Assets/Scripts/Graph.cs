6d3fe73115d88860952d325af402cb139e598667
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-02-19 20:21:18 -0800

    Made a point surface shader to colour points

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,14 +5,20 @@ public class Graph : MonoBehaviour {
 	[SerializeField]
 	Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+	int resolution = 10;
+
     void Awake () {
-        Vector3 position = Vector3.zero;
-        var scale = Vector3.one / 5f;
-		for (int i = 0; i < 10; i++) {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+		for (int i = 0; i < resolution; i++) {
 			Transform point = Instantiate(pointPrefab);
-            position.x = ((i + 0.5f) / 5f - 1f);
+            position.x = ((i + 0.5f) * step - 1f);
+            position.y = position.x * position.x * position.x;
 			point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
 		}
 	}
 }
